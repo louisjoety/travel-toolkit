e017b8bb27588e3a4a84e72c2a48da14ee64c57f
Louis Joe <kolouuis002@gmail.com>
2024-07-13 11:01:11 +0700

    logic of image to text included

--- app.py
+++ app.py
@@ -2,4 +2,20 @@ import cv2
 import pytesseract
 from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = r' C:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+def convert_image_to_text(image_path):
+    
+    image = cv2.imread(image_path)
+    
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    text = pytesseract.image_to_string(rgb_image)
+    
+    return text
+
+if __name__ == "__main__":
+    image_path = '' # TODO: Add file path to image
+    text = convert_image_to_text(image_path)
+    print("Extracted Text:")
+    print(text)
