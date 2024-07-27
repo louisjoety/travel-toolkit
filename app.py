be2f71eaa7882cfedfa53333ae008995ee35b5d6
Louis Joe <kolouis002@gmail.com>
2024-07-28 01:06:06 +0700

    API key inserted with currecny shown

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print('hello world')
+import freecurrencyapi
+from dotenv import load_dotenv
+import os
+
+
+API_KEY = os.getenv('FREECURRENCYAPI_KEY')
+client = freecurrencyapi.Client('API_KEY')
+
+# Load environment variables from .env file
+load_dotenv()
+
+result = client.currencies(currencies=['EUR', 'CAD'])
+print(result)
