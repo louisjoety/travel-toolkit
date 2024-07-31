4d55ff6352d5fefb7659760f228c01d5be2bf09e
Louis Joe <kolouis002@gmail.com>
2024-08-01 01:31:06 +0700

    added banner features

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { Lato } from "@next/font/google";
 import Logo from "./Logo.png";
+
+// Load the Lato font
+const lato = Lato({
+  subsets: ['latin'],
+  weight: ['400', '700'], // specify the weights you want to use
+});
 
 const Header = () => {
   return (
-    <nav className='fixed w-full h-24 shadow-xl bg-white'>
+    <nav className={`fixed w-full h-24 shadow-xl bg-white ${lato.className}`}>
       <div className='flex justify-between items-center h-full w-full px-4 2xl:px-16'>
         <Link href="/">
           <Image
@@ -17,7 +24,22 @@ const Header = () => {
             priority
           />
         </Link>
-        
+        <div>
+          <ul className="hidden sm:flex">
+            <Link href="/translator">
+              <li className="ml-10 uppercase hover:border-b text-xl">Image-to-Text</li>
+            </Link>
+            <Link href="/translator">
+              <li className="ml-10 uppercase hover:border-b text-xl">Translate</li>
+            </Link>
+            <Link href="/summarize">
+              <li className="ml-10 uppercase hover:border-b text-xl">Summarise</li>
+            </Link>
+            <Link href="/converter">
+              <li className="ml-10 uppercase hover:border-b text-xl">Convert Currency</li>
+            </Link>
+          </ul>
+        </div>
       </div>
     </nav>
   )
