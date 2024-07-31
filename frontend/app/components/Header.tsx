b586f4fee12f0fe6ba43af0e43400cba6972942a
Louis Joe <kolouis002@gmail.com>
2024-08-01 01:47:06 +0700

    hamburger icon added

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,8 +1,12 @@
+'use client';
+
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { Lato } from "@next/font/google";
 import Logo from "./Logo.png";
+import {AiOutlineMenu} from 'react-icons/ai'
+import { useState } from "react";
 
 // Load the Lato font
 const lato = Lato({
@@ -11,6 +15,12 @@ const lato = Lato({
 });
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleNav = () => {
+    setMenuOpen(!menuOpen)
+  }
+
   return (
     <nav className={`fixed w-full h-24 shadow-xl bg-white ${lato.className}`}>
       <div className='flex justify-between items-center h-full w-full px-4 2xl:px-16'>
@@ -24,7 +34,7 @@ const Header = () => {
             priority
           />
         </Link>
-        <div>
+        <div className="hidden sm:flex">
           <ul className="hidden sm:flex">
             <Link href="/translator">
               <li className="ml-10 uppercase hover:border-b text-xl">Image-to-Text</li>
@@ -40,6 +50,15 @@ const Header = () => {
             </Link>
           </ul>
         </div>
+        <div onClick={handleNav} className="sm:hidden cursor pl-24">
+          <AiOutlineMenu size={25} />
+        </div>
+      </div>
+      <div className={
+        menuOpen 
+        ? "fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+        : "fixed left-[100%] top-0 p-10 ease-in duration-500"
+      }>
       </div>
     </nav>
   )
