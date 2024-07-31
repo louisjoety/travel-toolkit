fe2c2fb4928d887ff054e563fb318f609d02266b
Louis Joe <kolouis002@gmail.com>
2024-08-01 01:24:10 +0700

    added the logo

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "./Logo.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav className='fixed w-full h-24 shadow-xl bg-white'>
+      <div className='flex justify-between items-center h-full w-full px-4 2xl:px-16'>
+        <Link href="/">
+          <Image
+            src={Logo}
+            alt="Logo"
+            width={50}
+            height={50}
+            className="cursor-pointer"
+            priority
+          />
+        </Link>
+        
+      </div>
+    </nav>
   )
 }
 
-export default Header
+export default Header;
