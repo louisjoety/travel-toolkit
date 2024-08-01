cc2dda4e9d4caae1e786f0d93c55c1169fbd8cb0
Louis Joe <kolouis002@gmail.com>
2024-08-02 00:10:10 +0700

    template in

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,12 +3,14 @@
 import { useRouter } from 'next/navigation';
 import "./globals.css";
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function Home() {
   const router = useRouter()
   return (
     <>
     <Header />
+    <Footer />
     </>
   );
 }
