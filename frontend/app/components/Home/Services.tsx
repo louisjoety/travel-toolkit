aec22a3ba077b16264f299ebbac7dc4e8d8e4cfb
Louis Joe <kolouis002@gmail.com>
2024-08-08 01:28:06 +0700

    remove redundancy

--- frontend/app/components/Home/Services.tsx
+++ frontend/app/components/Home/Services.tsx
@@ -15,9 +15,6 @@ type Props = {
 const Description = ({ activeImage, clickNext, clickPrev }: Props) => {
   return (
     <div className="grid place-items-start w-full bg-[#e7dfd9] relative md:rounded-tr-3xl md:rounded-br-3xl">
-      <div className="uppercase text-sm absolute right-4 top-2 underline-offset-4 underline">
-        Coffee Cafe
-      </div>
       {images.map((elem, idx) => (
         <div
           key={idx}
