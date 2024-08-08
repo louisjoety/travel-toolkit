0b49c125da5d5e7867329e189355f4141b5257b6
Louis Joe <kolouis002@gmail.com>
2024-08-08 23:18:28 +0700

    added the small inline text

--- frontend/app/components/Questions.tsx
+++ frontend/app/components/Questions.tsx
@@ -46,7 +46,7 @@ const Questions: React.FC = () => {
   return (
     <div className="py-24 px-8 max-w-5xl mx-auto flex flex-col md:flex-row gap-12">
       <div className="flex flex-col text-left basis-1/2">
-        <p className="inline-block font-semibold text-primary mb-4">Insurance FAQ</p>
+        <p className="inline-block font-semibold text-primary mb-4">Get answers to common questions about our app’s features and usage.</p>
         <p className="sm:text-4xl text-3xl font-extrabold text-base-content">Frequently Asked Questions</p>
       </div>
       <ul className="basis-1/2">
