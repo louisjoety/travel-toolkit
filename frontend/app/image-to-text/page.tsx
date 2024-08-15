5d98fc71af932d7d655721a080a6c6c51dd20b93
Louis Joe <kolouis002@gmail.com>
2024-08-15 23:50:31 +0800

    added new placeholder page

---
+++ frontend/app/image-to-text/page.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import "../globals.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Questions from "../components/Questions";
+
+export default function Translator() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const router = useRouter();
+
+  const handleNav = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <>
+      <Header menuOpen={menuOpen} handleNav={handleNav} />
+     <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold mb-8">Image-to-text</h1>
+      <p className="text-lg">LoremIpsum.</p>
+    </main>
+    <Questions />
+    <Footer />
+    </>
+  );
+}
